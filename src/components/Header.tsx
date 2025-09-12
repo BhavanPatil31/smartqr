b4185b8aec75d8d2e802e98047f3ebfdd741c60f
Bhavan Patil <bhavanspatil2004@gmail.com>
2025-09-12 17:10:00 +0000

    add bright colors (_for element div_)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,8 @@ export function Header({ children }: { children?: ReactNode }) {
       <div className="container flex h-16 items-center">
         <div className="mr-auto flex items-center space-x-2">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <QrCode className="h-6 w-6 text-foreground" />
-            <span className="font-bold text-xl text-foreground">QAttend</span>
+            <QrCode className="h-6 w-6 text-primary" />
+            <span className="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-400">QAttend</span>
           </Link>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
